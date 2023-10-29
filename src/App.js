1371a6ac5f7dd8c983ce090427c767ee707f2ddd
Meiwen Dong <meiwendong@Meiwens-iMac.local>
2023-10-29 12:31:47 -0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import 'split-pane-react/esm/themes/default.css';
+import React, { useState, useRef, useEffect } from 'react';
+import SplitPane, { Pane } from 'split-pane-react';
+import Right from './components/Right';
+import Left from './components/Left';
 
 function App() {
+  //const [sizes, setSizes] = React.useState([100, '8%', 'auto']);
+  const [sizes, setSizes] = useState([]);
+  const canvasRef = useRef(null)
+  const [imageTransfer, setImageTransfer] = useState(null);
+
+  // useEffect(() => {
+  //   console.log(imageTransfer.content)
+
+  // }, [imageTransfer])
+
+
+
+  const layoutCSS = {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh' }}>
+      <h1
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+      }}
+      >Contact support: notebooksy@bbnote.ca</h1>
+      <SplitPane
+        split='vertical'
+        sizes={sizes}
+        onChange={setSizes}
+      >
+        <Pane minSize='20%' maxSize='80%'>
+          <div style={{ ...layoutCSS, background: '#ddd' }}>
+            <Left send={setImageTransfer}
+            />
+          </div>
+        </Pane>
+        <Pane minSize='20%' maxSize='80%'>
+          <div style={{ ...layoutCSS, background: '#d5d7d9', overflowY: "scroll" }}>
+            <Right
+              receive={imageTransfer}
+            />
+          </div>
+        </Pane>
+      </SplitPane>
     </div>
   );
 }
+
 
 export default App;
